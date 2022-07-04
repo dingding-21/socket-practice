ebf070a1b32e86c10f2b199e659d90cd320bc270
dingding-21 <phj8221@naver.com>
2022-07-04 20:08:27 +0900

    [ADD] WebSocket events & messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,20 @@
-alert('hi');
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 서버를 연결했을 때
+socket.addEventListener('open', () => {
+  console.log('Connected to Server✅');
+});
+
+// 서버로부터 메세지를 받을 때
+socket.addEventListener('message', (message) => {
+  console.log('Just got this: ', message.data, 'from the server');
+});
+
+// 서버로부터 연결이 끊겼을 때
+socket.addEventListener('close', () => {
+  console.log('Disconnected to Server❌');
+});
+
+setTimeout(() => {
+  socket.send('hello from the browser!');
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -7,13 +7,27 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 // 같은 서버에서 http, webSocket 둘 다 작동시킴
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+// connection이 생기면 socket을 받는다
+wss.on('connection', (socket) => {
+  console.log('Connected to Browser✅');
+  // 브러우저의 연결이 끊겼을 때의 listener 등록
+  // 익명 함수 사용
+  socket.on('close', () => console.log('Disconnected from the Browser❌'));
+  // 특정 socket에서 메시지를 받았을 때 실행
+  socket.on('message', (message) => {
+    console.log(message.toString('utf8'));
+  });
+  // frontend로 메세지 전송
+  socket.send('hello');
+});
 
 server.listen(3000, handleListen);
