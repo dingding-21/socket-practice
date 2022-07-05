0f6c28fed5a44ec2dd5cf37930b5a5074135fa65
dingding-21 <phj8221@naver.com>
2022-07-06 03:27:37 +0900

    [FEAT] Chat with websockets

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,13 @@
+const messageList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  // JavaScript -> string
+  return JSON.stringify(msg);
+}
 
 // 서버를 연결했을 때
 socket.addEventListener('open', () => {
@@ -7,7 +16,9 @@ socket.addEventListener('open', () => {
 
 // 서버로부터 메세지를 받을 때
 socket.addEventListener('message', (message) => {
-  console.log('Just got this: ', message.data, 'from the server');
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 // 서버로부터 연결이 끊겼을 때
@@ -15,6 +26,19 @@ socket.addEventListener('close', () => {
   console.log('Disconnected to Server❌');
 });
 
-setTimeout(() => {
-  socket.send('hello from the browser!');
-}, 10000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  input.value = '';
+}
+
+messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -16,18 +16,30 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 // connection이 생기면 socket을 받는다
 wss.on('connection', (socket) => {
+  socket['nickname'] = 'Anon';
+  sockets.push(socket);
   console.log('Connected to Browser✅');
   // 브러우저의 연결이 끊겼을 때의 listener 등록
   // 익명 함수 사용
   socket.on('close', () => console.log('Disconnected from the Browser❌'));
   // 특정 socket에서 메시지를 받았을 때 실행
-  socket.on('message', (message) => {
-    console.log(message.toString('utf8'));
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg);
+
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case 'nickname':
+        // 받은 nickname을 socket에 넣어준다
+        socket['nickname'] = message.payload;
+    }
   });
-  // frontend로 메세지 전송
-  socket.send('hello');
 });
 
 server.listen(3000, handleListen);
