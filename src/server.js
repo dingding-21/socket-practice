ad7bb03955a88758e31ecd69febecab379540ed0
dingding-21 <phj8221@naver.com>
2022-07-06 13:47:39 +0900

    [FEAT] Count open rooms

--- src/server.js
+++ src/server.js
@@ -13,6 +13,22 @@ app.get('/*', (_, res) => res.redirect('/'));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
 wsServer.on('connection', (socket) => {
   socket['nickname'] = 'Anon';
   // socket에 있는 모든 event 보기
@@ -23,6 +39,7 @@ wsServer.on('connection', (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit('welcome', socket.nickname);
+    wsServer.sockets.emit('room_change', publicRooms());
   });
 
   // 클라이언트가 서버와 연결이 끊어지기 전
@@ -30,6 +47,10 @@ wsServer.on('connection', (socket) => {
     socket.rooms.forEach((room) => {
       socket.to(room).emit('bye', socket.nickname);
     });
+  });
+
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms());
   });
 
   socket.on('new_message', (msg, room, done) => {
